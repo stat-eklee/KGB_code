b2832df9605792331d023e703d6d13126275927d
eunkyeong <64254829+stat-eklee@users.noreply.github.com>
2025-03-17 21:05:32 +0900

    Update run.sh

--- nlgi_eval/run.sh
+++ nlgi_eval/run.sh
@@ -1,5 +1,5 @@
 path=$1
 python nli_eval.py --model_name_or_path ${path} --type webnlg ./data/webnlg.csv output_.json
 python nli_eval.py --model_name_or_path ${path} --type webnlg --no-templates ./data/webnlg.csv output_backoff.json
-python nli_eval.py --model_name_or_path ${path} --type e2e ./data/webnlg.csv output_e2e.json
-python nli_eval.py --model_name_or_path ${path} --type e2e --no-templates ./data/webnlg.csv output_e2e_backoff.json
+python nli_eval.py --model_name_or_path ${path} --type e2e ./data/e2e.tsv output_e2e.json
+python nli_eval.py --model_name_or_path ${path} --type e2e --no-templates ./data/e2e.tsv output_e2e_backoff.json
